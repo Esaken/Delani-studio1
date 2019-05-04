2c6abacd7b28da89d0afb0e4484c7d22062bfdc9
Esaken <kenesau4763@gmail.com>
2019-05-04 17:58:38 +0300

    button r refusing to hover

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+//hide();
+
+//$('#btn1').hover(function() {
+  //$('.para1').toggle();
+//})
+//$('#btn2').hover(function() {
+  ///$('.para2').toggle();
+//})
+//$('#btn3').hover(function() {
+  //$('.para3').toggle();
+//})
+function hide() {
+  $(getElementById(btn1).on('click', function(){
+    $getElementByClass(para1).hide();
+  })
+  (getElementById(btn2).on('click', function(){
+    $getElementByClass(para2).hide();
+  })
+  (getElementById(btn3).on('click', function(){
+    $getElementByClass(para3).hide();
+  })
+}
+
+
+
+
+
