0eac15876298ddbfc29392ae4a69fa8db77c6526
Esaken <kenesau4763@gmail.com>
2019-05-05 12:54:33 +0300

    inputted text in the images

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,7 @@
-//hide();
-
-//$('#btn1').hover(function() {
-  //$('.para1').toggle();
-//})
-//$('#btn2').hover(function() {
-  ///$('.para2').toggle();
-//})
-//$('#btn3').hover(function() {
-  //$('.para3').toggle();
-//})
-function hide() {
-  $(getElementById(btn1).on('click', function(){
-    $getElementByClass(para1).hide();
-  })
-  (getElementById(btn2).on('click', function(){
-    $getElementByClass(para2).hide();
-  })
-  (getElementById(btn3).on('click', function(){
-    $getElementByClass(para3).hide();
-  })
-}
-
-
-
-
-
+function function() {
+$(document).ready(function(){
+  $("para1").click(function(){
+    $(document.getElementById('btn1')).hide();
+  });
+});
+};
