a0aa98c6a2061e18354d0ee377a9d0157fbe21dc
Esaken <kenesau4763@gmail.com>
2019-05-06 08:21:02 +0300

    new js

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $(".thumbnails1").click(function() {
+      $(".caption1").toggle();
+});
+});
+$(document).ready(function(){
+  $(".thumbnails2").click(function() {
+      $(".caption2").toggle();
+});
+});
+$(document).ready(function(){
+  $(".thumbnails3").click(function() {
+      $(".caption3").toggle();
+});
+});
